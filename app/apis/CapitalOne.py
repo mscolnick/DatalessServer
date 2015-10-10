53300911b43c086e5088b483c3205d51bc88a93f
Shane Barratt <stbarratt@gmail.com>
2015-10-10 02:48:05 -0700

    fixed some apis

--- app/apis/CapitalOne.py
+++ app/apis/CapitalOne.py
@@ -1,39 +1,42 @@
 import requests
 import json
 
-#------------------------Capital One------------------------------------#
-#Get balance of an account
+# www.reimaginebanking.com
 
-cone_url = 'http://api.reimaginebanking.com'
-cone_key = '1366093bda72bf51ff7a0449c3e7f910'
+class CapitalOneApi:
+	def __init__(self):
+		self.cone_url = 'http://api.reimaginebanking.com'
+		self.cone_key = '1366093bda72bf51ff7a0449c3e7f910'
 
-def get_accounts():
-  r = requests.get('http://api.reimaginebanking.com/accounts?key='+cone_key)
-  return json.loads(r.content)
+	def get_accounts(self):
+    # Input: None
+    # Output: all account information (dict)
+	  r = requests.get(self.cone_url + '/accounts?key='+self.cone_key)
+	  return json.loads(r.content)
 
-def get_account(account_id):
-  r = requests.get(str(cone_url) + '/accounts/' + str(account_id) + "?key=" + cone_key)
-  return json.loads(r.content)
+	def get_account(self, account_id):
+    # Input: account_id
+    # Output: account information for a specific id (dict)
+	  r = requests.get(str(self.cone_url) + '/accounts/' + str(account_id) + "?key=" + self.cone_key)
+	  return json.loads(r.content)
 
-def get_balance(account_id):
-  return int(get_account(account_id)["balance"])
+	def get_balance(self, account_id):
+    # Input: account_id
+    # Output: balance (int)
+	  return int(self.get_account(account_id)["balance"])
 
-def get_customers():
-  #Returns python dictionary of customers
-  r = requests.get('http://api.reimaginebanking.com/customers?key='+cone_key)
-  return json.loads(r.content)
+	def get_customers(self):
+    # Input: None
+	  # Output: customers (dict)
+	  r = requests.get('http://api.reimaginebanking.com/customers?key=' + self.cone_key)
+	  return json.loads(r.content)
 
-# def create_account(account_id, customer_id, payload):
-#   r = requests.post(cone_url+, data=json.dumps(payload), headers={'content-type':'application/json'})
-#   return r.text
+if __name__ == '__main__':
+	c_one_api = CapitalOneApi()
+	print c_one_api.get_accounts()
+	print c_one_api.get_balance('560f0207f8d8770df0efa460')
+	print c_one_api.get_customers()
 
-# print get_accounts()
-# print get_balance('560f0207f8d8770df0efa460')
-# print get_customers()
 
-# create_account(1, {
-#           "type": "Savings",
-#           "nickname": "Shane",
-#           "rewards": 0,
-#           "balance": 0})
+
 
