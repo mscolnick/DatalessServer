acabb8efa34890657055332a2ce209e345e00b9c
Myles Scolnick <mscolnick@berkeley.edu>
2015-10-10 04:17:09 -0700

    stuff

--- app/apis/CapitalOne.py
+++ app/apis/CapitalOne.py
@@ -11,24 +11,53 @@ class CapitalOneApi:
 
 	def get_accounts(self):
     # Input: None
-    # Output: all account information (dict)
+    # Output: all account information (list of jsons)
+    # [
+    #   {
+    #     "rewards": 11870,
+    #     "customer_id": "560f0205f8d8770df0ef99da",
+    #     "type": "Credit Card",
+    #     "_id": "560f0207f8d8770df0efa460",
+    #     "balance": 17181,
+    #     "nickname": "Gerhardt's Account"
+    #   },
+    #   ...
+    # ]
 	  r = requests.get(self.cone_url + '/accounts?key='+self.cone_key)
 	  return json.loads(r.content)
 
 	def get_account(self, account_id):
     # Input: account_id
     # Output: account information for a specific id (dict)
+    # {
+    #   "rewards": 11870,
+    #   "customer_id": "560f0205f8d8770df0ef99da",
+    #   "type": "Credit Card",
+    #   "_id": "560f0207f8d8770df0efa460",
+    #   "balance": 17181,
+    #   "nickname": "Gerhardts Account"
+    # }
 	  r = requests.get(str(self.cone_url) + '/accounts/' + str(account_id) + "?key=" + self.cone_key)
 	  return json.loads(r.content)
-
-	def get_balance(self, account_id):
-    # Input: account_id
-    # Output: balance (int)
-	  return int(self.get_account(account_id)["balance"])
 
 	def get_customers(self):
     # Input: None
 	  # Output: customers (dict)
+    # [
+    #   {
+    #     "last_name": "OReilly",
+    #     "first_name": "Skylar",
+    #     "_id": "560f0205f8d8770df0ef99da",
+    #     "address": {
+    #       "city": "Swanton",
+    #       "street_name": "Coopers Lane",
+    #       "zip": "21561",
+    #       "state": "Maryland",
+    #       "street_number": "574"
+    #     }
+    #   },
+    #   ...
+    # ]
 	  r = requests.get('http://api.reimaginebanking.com/customers?key=' + self.cone_key)
 	  return json.loads(r.content)
 
@@ -37,7 +66,3 @@ if __name__ == '__main__':
 	print c_one_api.get_accounts()
 	print c_one_api.get_balance('560f0207f8d8770df0efa460')
 	print c_one_api.get_customers()
-
-
-
-
