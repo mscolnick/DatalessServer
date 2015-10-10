d247ed174decda3ef4c8a9d9c8111a4030b37515
Shane Barratt <stbarratt@gmail.com>
2015-10-10 03:23:01 -0700

    updated Sigimera

--- app/apis/Sigimera.py
+++ app/apis/Sigimera.py
@@ -4,24 +4,45 @@ import json
 #--------------------------Sigimera-----------------------------------#
 #get disaster information for the world or your location
 
+class SigimeraApi:
 
-sigimera_authentication_token = 'nTP7wzBk2nEos22_8DB_'
+  def __init__(self):
+    self.sigimera_authentication_token = 'nTP7wzBk2nEos22_8DB_'
 
-lat = 21.2222
-lon = -122.122
+  def get_disasters_in_radius(self, lat, lon, radius=500):
+    # Input: lat (int), lon(int), radius(int) in km
+    # Output: [] if no disasters, list of disasters in a x radius
+    try:
+      url = 'http://api.sigimera.org/v1/crises?auth_token=%s&lat=%s&lon=%s&radius=%s' % (self.sigimera_authentication_token, lat, lon, str(radius))
+      response = json.loads(requests.get(url).content)
+      disasters = []
+      for res in response:
+        disasters += [res['dc_description']]
+      return disasters
+    except:
+      return []
 
-def get_disasters(lat,lon):
-  try:
-    # url = 'http://api.sigimera.org/v1/crises?auth_token=%s&lat=%s&lon=%s&radius=500' % (sigimera_authentication_token, lat, lon)
-    url = 'http://api.sigimera.org/v1/crises?auth_token=%s' % (sigimera_authentication_token)
-    response = json.loads(requests.get(url).content)
-    disasters = []
-    for res in response:
-      disasters += [res['dc_description']]
-    return disasters
-  except:
-    return "no disasters"
+  def get_world_disasters(self):
+    # Input: None
+    # Output: [] if no disasters, list of disasters
+    try:
+      url = 'http://api.sigimera.org/v1/crises?auth_token=%s' % (self.sigimera_authentication_token)
+      response = json.loads(requests.get(url).content)
+      disasters = []
+      for res in response:
+        disasters += [res['dc_description']]
+      return disasters
+    except:
+      return []
 
-# for i in get_disasters(31.777,-122.123):
-#   print i
-#   print ""
+if __name__ == '__main__':
+  sigimera_api = SigimeraApi()
+  lat = 21.2222
+  lon = -122.122
+  for i in sigimera_api.get_disasters_in_radius(lat,lon,500):
+    print i
+    print ""
+
+  for i in sigimera_api.get_world_disasters():
+    print i
+    print ""
