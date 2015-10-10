0ed230f443206ce937495ba37f50cb1a09e6505d
Myles Scolnick <mscolnick@berkeley.edu>
2015-10-10 01:48:35 -0700

    moved db start

--- application.py
+++ application.py
@@ -1,11 +1,6 @@
-from app import create_app, setup_database
-import os
-from config.development import SQLALCHEMY_DATABASE_URI
+from app import create_app
 
-app = create_app('config.development')
+application = create_app('config.development')
 
 if __name__ == '__main__':
-    if not os.path.isfile(SQLALCHEMY_DATABASE_URI):
-      setup_database(app)
-    app.debug = True
-    app.run()
+    application.run()
