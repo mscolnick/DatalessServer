0e957a37f9b8c382fbbfa46b3868332c4cd5ef83
Myles Scolnick <mscolnick@berkeley.edu>
2015-10-10 22:30:32 -0700

    stuff

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,7 @@
 import re
 def cleanString(string):
   ascii_string = string.decode('unicode_escape').encode('ascii', 'ignore')
+  ascii_string = ascii_string.replace('"', "'")
   return re.sub('\\\u....','',ascii_string)
 
 def fix_number(number, size):
