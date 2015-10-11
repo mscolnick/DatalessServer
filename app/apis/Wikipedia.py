2af67ad2f5b7f7b3187ad20211839889f958c68c
Shane Barratt <stbarratt@gmail.com>
2015-10-11 05:18:14 -0700

    fixed Disambiguation Error

--- app/apis/Wikipedia.py
+++ app/apis/Wikipedia.py
@@ -2,19 +2,28 @@ import wikipedia
 import json
 
 class WikipediaApi:
-	def __init__(self):
-		pass
+  def __init__(self):
+    pass
 
-	def get_summary(self, topic, num_sentences=4):
-	# Input: topic (string), num_sentences (int)
-	# Output: string of topic
-		num_sentences = int(num_sentences)
-		result = {
-			"summary": wikipedia.summary(topic, sentences=num_sentences).replace('"',"'"),
-			"topic": topic
-		}
-		return json.dumps(result)
+  def get_summary(self, topic, num_sentences=4):
+  # Input: topic (string), num_sentences (int)
+  # Output: string of topic
+    num_sentences = int(num_sentences)
+    try:
+      summary = wikipedia.summary(topic, sentences=num_sentences)
+    except Exception as e: # in case disambiguation happens
+      try:
+        topic = e.options[0] 
+        summary = wikipedia.summary(topic, sentences=num_sentences)
+      except:
+        return {"summary": "no such article exists", "topic": topic}
+
+    result = {
+      "summary": summary.replace('"',"'"),
+      "topic": topic
+    }
+    return json.dumps(result)
 
 if __name__ == '__main__':
-	wapi = WikipediaApi()
-	print wapi.get_summary('sonny dykes', 5)
+  wapi = WikipediaApi()
+  print wapi.get_summary('test', 5)
